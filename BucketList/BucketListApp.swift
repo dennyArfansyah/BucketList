0d37eeed5a93095c18ed7aafc208e164e912412d
dennyArfansyah <dennyarfansyah@gmail.com>
2023-01-31 14:51:44 +0700

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Denny Arfansyah on 31/01/23.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
